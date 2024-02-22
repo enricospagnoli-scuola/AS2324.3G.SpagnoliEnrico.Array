8391ff499490e7987befe53ed4698e17ac0f7edb
Enrico Spagnoli <espagnoli07@gmail.com>
2024-02-22 19:52:21 +0100

    Feat funzione "Ordina".

--- Program.cs
+++ Program.cs
@@ -12,6 +12,12 @@ class Program
         double media = 0, min = 0, max = 0;
         Statistiche(ref pesi, ref media, ref min, ref max);
         Console.WriteLine($"Media: {media}, Minimo: {min}, Massimo: {max}");
+        Ordina(ref pesi, ref eta);
+        Console.WriteLine("Contenuto dei vettori ordinati per età:");
+        for (int i = 0; i < dim; i++)
+        {
+            Console.WriteLine($"Peso: {pesi[i]}, Età: {eta[i]}");
+        }
     }
     static void CaricaVettori(ref double[] pesi, ref int[] eta, int elementi)
     {
@@ -36,5 +42,24 @@ class Program
                 max = peso;
         }
         media = somma / pesi.Length;
+    }
+    static void Ordina(ref double[] pesi, ref int[] eta)
+    {
+        for (int i = 0; i < pesi.Length - 1; i++)
+        {
+            for (int j = i + 1; j < pesi.Length; j++)
+            {
+                if (eta[i] > eta[j])
+                {
+                    double tempPeso = pesi[i];
+                    pesi[i] = pesi[j];
+                    pesi[j] = tempPeso;
+
+                    int tempEta = eta[i];
+                    eta[i] = eta[j];
+                    eta[j] = tempEta;
+                }
+            }
+        }
     }
 }
