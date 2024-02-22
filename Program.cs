d2e1f9b79c18e6649c001ed9447083e9dc7f5c8c
Enrico Spagnoli <espagnoli07@gmail.com>
2024-02-22 19:51:41 +0100

    Feat funzione "Statistiche".

--- Program.cs
+++ Program.cs
@@ -9,6 +9,9 @@ class Program
         double[] pesi = new double[dim];
         int[] eta = new int[dim];
         CaricaVettori(ref pesi, ref eta, dim);
+        double media = 0, min = 0, max = 0;
+        Statistiche(ref pesi, ref media, ref min, ref max);
+        Console.WriteLine($"Media: {media}, Minimo: {min}, Massimo: {max}");
     }
     static void CaricaVettori(ref double[] pesi, ref int[] eta, int elementi)
     {
@@ -18,5 +21,20 @@ class Program
             pesi[i] = random.Next(50, 101);
             eta[i] = random.Next(18, 100);
         }
+    }
+    static void Statistiche(ref double[] pesi, ref double media, ref double min, ref double max)
+    {
+        double somma = 0;
+        min = pesi[0];
+        max = pesi[0];
+        foreach (double peso in pesi)
+        {
+            somma += peso;
+            if (peso < min)
+                min = peso;
+            if (peso > max)
+                max = peso;
+        }
+        media = somma / pesi.Length;
     }
 }
